dead99074cb1d298a6a87bcb7c3f6cebe3589b83
longnd25 <71799172+longnd25@users.noreply.github.com>
2020-10-28 17:49:59 +0700

    Add files via upload

---
+++ 3.PROJECT/includes/public/navbar.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+	<div class="logo_div">
+		<h1>CSE.TLU</h1>
+	</div>
+	<ul>
+	  <li><a class="active" href="index.php">Trang chủ</a></li>
+	  
+	  <li><a href="forum.php">Diễn đàn</a></li>
+	  <li><a href="login.php">Login</a></li>
+	  <li><a href="create_post.php">Tao cau hoi</a></li>
+	  
+	</ul>
+</div>
